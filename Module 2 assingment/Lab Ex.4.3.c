6f1c459d79eb90e9111fd9f29f18e6f40eb3c9e6
Keyur Joshi <joshikeyur.work@gmail.com>
2026-02-06 15:38:25 +0530

    Add files via upload

---
+++ Module 2 assingment/Lab Ex.4.3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    float a, b, h;
+    float areaSquare, areaCube, areaTriangle;
+
+    // Area of Square
+    printf("Enter side of the square: ");
+    scanf("%f", &a);
+    areaSquare = a * a;
+    printf("Area of Square = %.2f\n", areaSquare);
+
+    // Area of Cube (Total Surface Area)
+    areaCube = 6 * a * a;
+    printf("Area of Cube (Total Surface Area) = %.2f\n", areaCube);
+
+    // Area of Triangle
+    printf("\nEnter base of the triangle: ");
+    scanf("%f", &b);
+    printf("Enter height of the triangle: ");
+    scanf("%f", &h);
+    areaTriangle = 0.5 * b * h;
+    printf("Area of Triangle = %.2f\n", areaTriangle);
+
+    return 0;
+}
