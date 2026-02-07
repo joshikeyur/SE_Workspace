237f56b8931a40ad6900295f9caf9bf08a4a0084
Keyur Joshi <joshikeyur.work@gmail.com>
2026-02-07 12:50:58 +0530

    Add files via upload

---
+++ Module 2 assingment/Lab ex.con.2.1.c
@@ -0,0 +1,37 @@
+
+// Grade calculator // 
+
+/*Marks > 90: Grade A
+Marks > 75 and <= 90: Grade B
+Marks > 50 and <= 75: Grade C
+Marks <= 50: Grade D*/
+
+#include <stdio.h>
+#include <conio.h>
+ int main ()
+ {
+   int marks ;
+   
+   printf("\n Enter your marks ");
+   scanf ("%d", &marks);
+    
+    if (marks > 90)
+    {
+        printf ("\n congratulations Grade : A"); // I added an extra  Grade A+//
+    }
+    else if (marks>75 && marks<=90)
+     {
+         printf ("\n congratulations Grade B");
+     }
+     else if ( marks >50 &&  marks <=75 )
+     {
+         printf ("\n congratulations Grade C");
+     }
+     else if ( marks<= 50)
+     {
+         printf ("\n congratulations Grade D");
+     };
+   
+         
+     return 0 ; 
+ }
